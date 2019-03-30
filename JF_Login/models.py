f3b5954764dbab2a64e1db5ab8b93031faebc37d
Caroline Clara <carolclara97@hotmail.com>
2019-03-30 20:20:05 -0300

    Cria models de usuário

--- JF_Login/models.py
+++ JF_Login/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 
+class User(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250, blank=False, null=False,)
+    username = models.CharField(max_length=30, blank=False, null=False, unique=True)
+    password = models.CharField(max_lenght=32, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False, unique=True)
+
+
+class Teacher(User):
+    pass
+
+
+class Student(User):
+    pass
