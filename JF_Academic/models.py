988c12a32ae4aec5c1788bbf622a19f5b634493b
Caroline Clara <carolclara97@hotmail.com>
2019-03-30 21:28:44 -0300

    Cria modelos de acadêmico

--- JF_Academic/models.py
+++ JF_Academic/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from JF_Login.models import Student
+
+
+class Class(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    student = models.ManyToManyField(Student)
+
+
+class Course(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    course = models.ManyToManyField(Class)
+
+
+class Program(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    course = models.ManyToManyField(Course)
+
+
+class Unit(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    program = models.ManyToManyField(Program)
