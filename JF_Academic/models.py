3ca889e56f81f15297f7075dc498717ee484443e
Caroline Clara <carolclara97@hotmail.com>
2019-03-30 23:13:27 -0300

    Cria models e as relaciona

--- JF_Academic/models.py
+++ JF_Academic/models.py
@@ -6,26 +6,26 @@ from JF_Login.models import Student
 class Class(models.Model):
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField()
     student = models.ManyToManyField(Student)
 
 
 class Course(models.Model):
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField()
     course = models.ManyToManyField(Class)
 
 
 class Program(models.Model):
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField()
     course = models.ManyToManyField(Course)
 
 
 class Unit(models.Model):
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     program = models.ManyToManyField(Program)
--- JF_Login/models.py
+++ JF_Login/models.py
@@ -6,10 +6,10 @@ from JF_Academic.models import Program
 class User(models.Model):
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=250, blank=False, null=False,)
-    username = models.CharField(max_length=30, blank=False, null=False, unique=True)
+    name = models.CharField(max_length=250)
+    username = models.CharField(max_length=10, unique=True)
     password = models.CharField(max_lenght=32, blank=False, null=False)
-    email = models.EmailField(blank=False, null=False, unique=True)
+    email = models.EmailField(unique=True)
 
 
 class Teacher(User):
