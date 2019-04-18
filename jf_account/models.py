6ea5642155becf5961f0c19778505bd4d73e96ad
Caroline Clara <carolclara97@hotmail.com>
2019-04-17 22:43:02 -0300

    Ajusta css para tornar responsivo o formulario Ajusta o django para funcionar com a model

--- jf_account/models.py
+++ jf_account/models.py
@@ -1,23 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-
-    id = models.AutoField(primary_key=True)
+class User(AbstractUser):
     name = models.CharField(max_length=250)
     username = models.CharField(max_length=10, unique=True)
-    password = models.CharField(max_length=32)
     email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
 
 class Teacher(models.Model):
 
     id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
 
 class Student(models.Model):
 
     id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     program = models.ForeignKey('jf_academic.Program', on_delete=models.PROTECT)
