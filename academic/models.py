c73ca67d995afc2f68a8b7b49013b8db8284e202
Caroline Clara <carolclara97@hotmail.com>
2019-04-28 19:13:11 -0300

    Altera relacionamento entre as models de disciplina e turma

--- academic/models.py
+++ academic/models.py
@@ -4,22 +4,22 @@ from judgment_facts.models import JudgmentFacts
 from account.models import Student
 
 
+class Course(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
 class Group(models.Model):
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=60)
     student = models.ManyToManyField(Student)
     judgment_facts = models.ManyToManyField(JudgmentFacts)
-
-    def __str__(self):
-        return self.name
-
-
-class Course(models.Model):
-
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=60)
-    group = models.ManyToManyField(Group)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
